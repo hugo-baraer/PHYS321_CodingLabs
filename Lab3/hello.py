22c3bc4f51ab50b14ab8139820b5a40e5476b1b0
hugo-baraer <hugo.baraer@mail.mcgil.ca>
2021-01-28 15:36:46 -0500

    added a space to counter Luca accidental pull

--- Lab3/hello.py
+++ Lab3/hello.py
@@ -1,7 +1,7 @@
 print('Hello world!')
 # I am adding this variable to test what a merge conflict looks like
 y = 8
-print('Hello! My name is Elsa, and the cold never bothered me anyway.')
+print('Hello! My name is Elsa, and the cold never bothered me anyway. ')
 x = 5 #not 8
 
 
