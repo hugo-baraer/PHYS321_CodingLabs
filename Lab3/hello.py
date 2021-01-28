903889e1c9fad28188abc465102b3bc4b19aa230
hugo-baraer <hugo.baraer@mail.mcgil.ca>
2021-01-28 15:27:30 -0500

    adjusted some comments

--- Lab3/hello.py
+++ Lab3/hello.py
@@ -2,6 +2,6 @@ print('Hello world!')
 # I am adding this variable to test what a merge conflict looks like
 y = 8
 print('Hello! My name is Anna. Do you wanna build a snowman?')
-x = 5 #not 9 you german
+x = 5 #not 8
 
 
